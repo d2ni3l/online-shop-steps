d6d3cc982469de7314e25c3d6ed18b8e460bc7f7
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2023-01-19 12:12:15 +0100

    vite and tailwind css setup

---
+++ project-7/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="App">
+     <div className="flex justify-center pt-5">
+      <h1 className="text-c underline font-bold text-4xl text-indigo-700">Hello world</h1>
+     </div>
+    </div>
+  )
+}
+
+export default App
