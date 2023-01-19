5a55a099ffba3deac6bbfdb37393d3723ba32887
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2023-01-19 12:33:58 +0100

    added design template

--- project-7/src/App.jsx
+++ project-7/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
      <div className="flex justify-center pt-5">
-      <h1 className="text-c underline font-bold text-4xl text-indigo-700">Hello world</h1>
+      <h1 className=" underline font-bold text-4xl text-indigo-700">Hello world</h1>
      </div>
     </div>
   )
